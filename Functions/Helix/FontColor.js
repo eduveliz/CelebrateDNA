9f30b533988e624d8421c416aa5bb3d981a03bfc
eduveliz <eduardoveliz2612@gmail.com>
2019-09-16 10:33:35 -0500

    color

--- Functions/Helix/FontColor.js
+++ Functions/Helix/FontColor.js
@@ -1,9 +1,15 @@
 module.exports = fontColor = (color) => {
     if (color === "Navy ") {
         return "white";
+    } else if (color === "Navy") {
+        return "white";
     } else if (color === "Black ") {
         return "white";
+    } else if (color === "Black") {
+        return "white";
     } else if (color === "Steel Blue ") {
+        return "white";
+    } else if (color === "Steel Blue") {
         return "white";
     } else {
         return "#58585B";
