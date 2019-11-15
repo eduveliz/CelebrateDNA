7184ea2cafd381255ac8020eef03d04fc8f841d9
eduveliz <eduardoveliz2612@gmail.com>
2019-11-15 16:41:35 -0500

    Helix Mugs Printfull Complete

--- Functions/Previews/MapsMug/compassSelector.js
+++ Functions/Previews/MapsMug/compassSelector.js
@@ -1,9 +1,21 @@
-module.exports = image = (headline) => {
-    if (headline === "Celebrating My DNA!") {
-        return "https://moolab.ml/HelixHorizontal/Celebrating.png";
-    } else if (headline === "Celebrating My DNA") {
-        return "https://moolab.ml/HelixHorizontal/Rockin.png";
-    } else if (headline === "My DNA Rocks") {
-        return "https://moolab.ml/HelixHorizontal/Roots.png";
+module.exports = image = (headline, font) => {
+    if (headline === "Celebrating / My DNA!" && font === "Noteworthy") {
+        return "https://moolab.ml/mug/Celebrating.png";
+    } else if (headline === "Rockin'/My DNA!" && font === "Noteworthy") {
+        return "https://moolab.ml/mug/Rockin.png";
+    } else if (headline === "First name / DNA" && font === "Noteworthy") {
+        return "https://moolab.ml/mug/Roots.png";
+    } else if (headline === "Celebrating / My DNA!" && font === "MyriadPro-Bold") {
+        return "https://moolab.ml/mug/mpCelebrating.png";
+    } else if (headline === "Rockin'/My DNA!" && font === "MyriadPro-Bold") {
+        return "https://moolab.ml/mug/mpRockin.png";
+    } else if (headline === "First name / DNA" && font === "MyriadPro-Bold") {
+        return "https://moolab.ml/mug/mpPersonalized.png";
+    } else if (headline === "First name / DNA" && font === "Funnier") {
+        return "https://moolab.ml/mug/fCelebrating.png";
+    } else if (headline === "Rockin'/My DNA!" && font === "Funnier") {
+        return "https://moolab.ml/mug/fRockin.png";
+    } else if (headline === "First name / DNA" && font === "Funnier") {
+        return "https://moolab.ml/mug/fPersonalized.png";
     }
 };
